28e12796b7bef6d042dbe75ff1534622d2555477
Regina Wanjiru Kariuki <wanjiruregina@gmail.com>
2025-03-07 17:35:14 +0300

    arrow functions

---
+++ index.js
@@ -0,0 +1,8 @@
+const divide = function(){
+    return 2000/100
+}
+const square = x => x * x;
+const add = (y, z) => y + z;
+ console.log(divide(2000, 100));
+ console.log(square( 50));
+ console.log(add(50, 100));
